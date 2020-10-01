676cd7875d9b32fdca7f35e5618f1b48d75cf5ff
webtoor <fityanalimunshi@gmail.com>
2020-10-01 18:43:07 +0700

    create model User

---
+++ backend/models/User.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"log"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// TableName ...
+func (User) TableName() string {
+	return "user"
+}
+
+// User ...
+type User struct {
+	UserID    uint      `gorm:"primary_key;column:user_id" json:"user_id"`
+	Username  string    `gorm:"unique;column:username"  json:"username" binding:"required"`
+	Email     string    `gorm:"unique;column:email"  json:"email" binding:"required,email"`
+	Password  string    `gorm:"column:password;default:null" json:"password" binding:"required"`
+	Saldo     uint      `gorm:"column:saldo"  json:"saldo" binding:"required"`
+	CreatedAt time.Time `gorm:"column:created_at;type:datetime"  json:"created_at"`
+	UpdatedAt time.Time `gorm:"column:updated_at;type:datetime"  json:"updated_at"`
+}
+
+// HashAndSalt ...
+func HashAndSalt(pwd string) string {
+	hash, err := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.DefaultCost)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return string(hash)
+}
--- backend/server.go
+++ backend/server.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	//"log"
+
+	//"github.com/gin-gonic/autotls"
+	"github.com/joho/godotenv"
+	"github.com/webtoor/test-indodax-go/backend/configs"
 	"github.com/webtoor/test-indodax-go/backend/routers"
 )
 
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+}
 func main() {
-
+	configs.InitDatabase()
+	defer configs.DB.Close()
 	r := routers.SetupRouter()
-
-	/* go func() {
-		go r.Run(":8080")
-	}() */
 
 	//log.Fatal(autotls.Run(r, "api.vuenic.com"))
 	r.Run(":8080")
